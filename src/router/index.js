ca90cb50a39f986b8f8cfa7bafd5146ae8eaf665
unknown <your-github-email@example.com>
2026-08-04 17:59:07 +0100

    fix: adjustment on scroll behaviour

--- src/router/index.js
+++ src/router/index.js
@@ -3,38 +3,43 @@ import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  
+
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0, left: 0 }
+    }
+  },
+
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: HomeView
     },
     {
       path: '/about',
       name: 'about',
-     
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../views/AboutView.vue')
     },
-        {
+    {
       path: '/term',
       name: 'term',
-     
-      component: () => import('../views/TermView.vue'),
+      component: () => import('../views/TermView.vue')
     },
-
-         {
+    {
       path: '/privacy',
       name: 'privacy',
-      component: () => import('../views/PrivacyView.vue'),
+      component: () => import('../views/PrivacyView.vue')
     },
-
-         {
+    {
       path: '/download',
       name: 'download',
-      component: () => import('../views/DownloadView.vue'),
+      component: () => import('../views/DownloadView.vue')
     }
-    
-  ],
+  ]
 })
 
 export default router
